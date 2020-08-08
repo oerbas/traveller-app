880a89e9e036aa9f8f1f92bf74c01c0d97b23731
oerbas <orhanerba@icloud.com>
2020-08-08 21:40:29 +0300

    uploaded files

---
+++ travellerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  travellerApp
+//
+//  Created by Orhan Erbas on 8.08.2020.
+//  Copyright © 2020 Orhan Erbas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
